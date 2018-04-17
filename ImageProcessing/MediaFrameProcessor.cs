567c6c95c1c284df3b41768d15462af88183c0ca
Firifire <firifire@gmail.com>
2018-04-17 15:31:31 +0200

    Added some resolution control (Untested)

--- ImageProcessing/MediaFrameProcessor.cs
+++ ImageProcessing/MediaFrameProcessor.cs
@@ -7,6 +7,9 @@
     using Windows.Media.Capture;
     using Windows.Media.Capture.Frames;
     using Windows.Media.Devices;
+    using Windows.Media.MediaProperties;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public abstract class MediaCaptureFrameProcessor<T> : IDisposable
     {
@@ -44,6 +47,12 @@
                   {
                       this.mediaCapture = await this.CreateMediaCaptureAsync();
                   }
+
+                  var allStreamProperties =
+        mediaCapture.VideoDeviceController.GetAvailableMediaStreamProperties(MediaStreamType.VideoPreview).Select(x => new StreamPropertiesHelper(x));
+                  allStreamProperties = allStreamProperties.OrderByDescending(x => x.Height * x.Width);
+                  await mediaCapture.VideoDeviceController.SetMediaStreamPropertiesAsync(MediaStreamType.VideoPreview, allStreamProperties.ElementAt(0).EncodingProperties);
+
                   var mediaFrameSource = this.mediaCapture.FrameSources[
               this.mediaFrameSourceFinder.FrameSourceInfo.Id];
 
@@ -113,6 +122,107 @@
                 this.mediaCapture = null;
             }
         }
+
+        class StreamPropertiesHelper
+        {
+            private IMediaEncodingProperties _properties;
+
+            public StreamPropertiesHelper(IMediaEncodingProperties properties)
+            {
+                if (properties == null)
+                {
+                    throw new ArgumentNullException(nameof(properties));
+                }
+
+                // This helper class only uses VideoEncodingProperties or VideoEncodingProperties
+                if (!(properties is ImageEncodingProperties) && !(properties is VideoEncodingProperties))
+                {
+                    throw new ArgumentException("Argument is of the wrong type. Required: " + typeof(ImageEncodingProperties).Name
+                        + " or " + typeof(VideoEncodingProperties).Name + ".", nameof(properties));
+                }
+
+                // Store the actual instance of the IMediaEncodingProperties for setting them later
+                _properties = properties;
+            }
+
+            public uint Width
+            {
+                get
+                {
+                    if (_properties is ImageEncodingProperties)
+                    {
+                        return (_properties as ImageEncodingProperties).Width;
+                    }
+                    else if (_properties is VideoEncodingProperties)
+                    {
+                        return (_properties as VideoEncodingProperties).Width;
+                    }
+
+                    return 0;
+                }
+            }
+
+            public uint Height
+            {
+                get
+                {
+                    if (_properties is ImageEncodingProperties)
+                    {
+                        return (_properties as ImageEncodingProperties).Height;
+                    }
+                    else if (_properties is VideoEncodingProperties)
+                    {
+                        return (_properties as VideoEncodingProperties).Height;
+                    }
+
+                    return 0;
+                }
+            }
+
+            public uint FrameRate
+            {
+                get
+                {
+                    if (_properties is VideoEncodingProperties)
+                    {
+                        if ((_properties as VideoEncodingProperties).FrameRate.Denominator != 0)
+                        {
+                            return (_properties as VideoEncodingProperties).FrameRate.Numerator /
+                                (_properties as VideoEncodingProperties).FrameRate.Denominator;
+                        }
+                    }
+
+                    return 0;
+                }
+            }
+
+            public double AspectRatio
+            {
+                get { return Math.Round((Height != 0) ? (Width / (double)Height) : double.NaN, 2); }
+            }
+
+            public IMediaEncodingProperties EncodingProperties
+            {
+                get { return _properties; }
+            }
+
+            public string GetFriendlyName(bool showFrameRate = true)
+            {
+                if (_properties is ImageEncodingProperties ||
+                    !showFrameRate)
+                {
+                    return Width + "x" + Height + " [" + AspectRatio + "] " + _properties.Subtype;
+                }
+                else if (_properties is VideoEncodingProperties)
+                {
+                    return Width + "x" + Height + " [" + AspectRatio + "] " + FrameRate + "FPS " + _properties.Subtype;
+                }
+
+                return String.Empty;
+            }
+
+        }
+
         Action<VideoDeviceController> videoDeviceControllerInitialiser;
         string mediaEncodingSubtype;
         MediaFrameSourceFinder mediaFrameSourceFinder;
